dc2ab2651c0c6a8a1a0d61d4c8067111c9e8a2c4
Viplow <viplow.291@gmail.com>
2020-10-31 12:20:48 +0530

    Added UI

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
+    const [isExpanded,setExpanded]=useState(false);
     const [note,setNote]=useState({
         title:"",
         content:""
@@ -23,12 +27,15 @@ function CreateArea(props) {
         event.preventDefault();
 
     }
+    function expand(){
+      setExpanded(true);
+    }
   return (
     <div>
-      <form>
-        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
-        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
-        <button onClick={handleClick}>Add</button>
+      <form className="create-note">
+        {isExpanded && (<input name="title" value={note.title} onChange={handleChange} placeholder="Title" />)}
+        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows={isExpanded ? 3 : 1} onClick={expand}/>
+        <Zoom in={isExpanded}><Fab onClick={handleClick}><AddIcon/></Fab></Zoom>
       </form>
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
 function Header(){
-    return(<header><h1>Keeper App</h1></header>);
+    return(<header><h1><HighlightIcon/> Keeper App</h1></header>);
 }
 export default Header;
